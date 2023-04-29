15319ca42e282e5f79e13ca05d4da35dc1276e3d
Steven <wusteven815@gmail.com>
2023-04-29 12:17:53 -0400

    Add Discord base

---
+++ libs/core/discord.ts
@@ -0,0 +1,29 @@
+//% color="#3d55f0" icon="\uf04b"
+//% groups="['Client', 'Events']"
+namespace discord {
+
+    //% block="run bot with token $token"
+    //% group="Client"
+    export function run(token: string) { }
+
+    //% block="on guild channel delete $channel"
+    //% draggableParameters
+    //% group="Events"
+    export function onGuildChannelDelete(handler: (channel: Channel) => void) { }
+
+    //% block="on guild channel create $channel"
+    //% draggableParameters
+    //% group="Events"
+    export function onGuildChannelCreate(handler: (channel: Channel) => void) { }
+    
+    //% block="on guild channel update from $before to $after"
+    //% draggableParameters
+    //% group="Events"
+    export function onGuildChannelUpdate(handler: (before: Channel, after: Channel) => void) { }
+
+    //% block="on message $message"
+    //% draggableParameters
+    //% group="Events"
+    export function onMessage(handler: (message: Message) => void) { }
+
+}
