e202b5c1b113b1e3ee004d7fe3c1f73024d0c6ef
Steven <wusteven815@gmail.com>
2023-04-30 01:49:05 -0400

    Fix group label

--- libs/core/discordFile.ts
+++ libs/core/discordFile.ts
@@ -1,8 +1,9 @@
-//% block="Discord Colour" color="#3d55f0" icon="\uf1fc"
-namespace discordColour {
+//% block="Discord File" color="#3d55f0" icon="\uf0c6"
+namespace discordFile {
 
     //% block="create file  | - file path $fp - file name $file_name - spoiler $spoiler"
     //$ spoiler.defl=false
+    //% blockSetVariable=file
     //% inlineInputMode=external
     export function createFile(fp: string, file_name: string, spoiler: boolean): File {
         return undefined
